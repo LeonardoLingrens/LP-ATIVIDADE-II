27feb52768ad5e950449f88a20cf4d4566d94434
LeonardoLingrens <143642029+LeonardoLingrens@users.noreply.github.com>
2023-11-07 16:21:33 -0300

    Add files via upload

---
+++ atv12.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+
+int main () {
+
+    setlocale(LC_ALL,"portuguese");
+
+    //Definindo Variáveis
+    float notas = 0;
+    float media = 0;
+    int quantidadeDeNotas;
+    int contador = 1;
+    int soma = 0;
+    int i;
+
+    //Solicitando dados
+    printf("Digite a quantidade de notas que serão escritas: ");
+    scanf("%i", &quantidadeDeNotas);
+
+    for ( i = 0; i < quantidadeDeNotas; i++)
+    {
+        printf("Informe a %i° nota: ",contador);
+        scanf("%f", &notas);
+        soma += notas; 
+        contador++;
+    }
+        media = soma / quantidadeDeNotas;
+
+        printf("Sua Média é: %.1f",media);
+    
+    
+    return 0;
+}
